61af7406c25d8ca506a1adba06098f0a7b26463c
gcesar99 <104658812+gcesar99@users.noreply.github.com>
2024-04-15 14:57:46 -0500

    Update geoReferenciacionFuncional.py

--- geoReferenciacionFuncional.py
+++ geoReferenciacionFuncional.py
@@ -91,4 +91,4 @@ def update_graph(selected_projects, selected_localities):
 if __name__ == '__main__':
     #app.run_server(debug=True)
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8888)
