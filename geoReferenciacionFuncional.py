2e02e18b8ad2b6f474fb78bbfa53273b3f4637e6
gcesar99 <104658812+gcesar99@users.noreply.github.com>
2024-04-15 12:36:20 -0500

    Update geoReferenciacionFuncional.py

--- geoReferenciacionFuncional.py
+++ geoReferenciacionFuncional.py
@@ -88,4 +88,6 @@ def update_graph(selected_projects, selected_localities):
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    #app.run_server(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
